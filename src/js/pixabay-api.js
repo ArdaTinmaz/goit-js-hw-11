f9fce97cc9d0a0b0589a9076f8a044b45cebee13
arda_tinmaz <tnmzarda@gmail.com>
2025-05-16 12:35:00 +0300

    fixed

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,67 +1,16 @@
-import { renderImages, renderNoResults, clearGallery } from "./render-functions.js";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import axios from 'axios';
 
-const baseUrl = `https://pixabay.com/api/`;
-const apiKey = `50314291-2088fdeaba0ad7602a74f6941`;
+const API_KEY = '50314291-2088fdeaba0ad7602a74f6941';
+const BASE_URL = 'https://pixabay.com/api/';
 
-const searchForm = document.getElementById("searchForm");
-const gallery = document.querySelector(".gallery");
+export function fetchImages(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
 
-
-const loader = document.getElementById('loader');
-function showLoader() {
-  loader.classList.remove('hidden');
+  return axios.get(BASE_URL, { params });
 }
-function hideLoader() {
-  loader.classList.add('hidden');
-}
-
-
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  clearGallery();
-
-  const searchWord = document.getElementById("searchInput").value.trim();
-
-    showLoader();
-    
-    if (!searchWord) {
-        hideLoader();
-    
-    return;
-  }
-
-  const params = new URLSearchParams({
-    key: apiKey,
-    q: searchWord,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: "true",
-  });
-
-  const url = `${baseUrl}?${params.toString()}`;
-   
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      setTimeout(() => {
-        if (data.hits.length === 0) {
-          renderNoResults();
-        } else {
-          renderImages(data.hits);
-        }
-        hideLoader();
-      }, 1000);
-    })
-    .catch(error => {
-      iziToast.error({
-        title: "Hata",
-        message: "API isteğinde hata oluştu.",
-        position: "topRight",
-      });
-      console.error("Fetch error:", error);
-      hideLoader();
-    });
-});
